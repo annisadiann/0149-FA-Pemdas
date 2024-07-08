6ff949e292f86834266e7f8f33a1af47c4ca8c25
ParadigmaOOP2_0149 <annisadian0412@gmail.com>
2024-07-08 19:07:26 +0700

    menambahkan variabel lain selain float presensi;

--- UAS/UAS.cpp
+++ UAS/UAS.cpp
@@ -3,6 +3,11 @@ using namespace std;
 class MataKuliah
 {
 private: // isi dengan access modifier yang dibutuhkan
-
+	float presensi;
+	// tambahkan dengan variabel lain yang dibutuhkan dibawah ini
+	float activity;
+	float exercise;
+	float tugasakhir;
 public:
+
 };
